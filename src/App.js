df41908a31788bdda39036c8a4281b8d4a2a63dd
Vishal Jani <vishljani123432@gmail.com>
2022-06-26 22:32:32 +0530

    add some code

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload added.
+          Edit <code>src/App.js</code> and save to reload added new data to check build.
         </p>
         <a
           className="App-link"
