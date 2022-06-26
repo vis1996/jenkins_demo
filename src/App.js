d09de4bb9dd419c21e6c73bf485b31cceece60a4
vis1996 <vishaljani123432@gmail.com>
2022-06-26 23:21:19 +0530

    added some new code

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload added new data to check build.
+          Edit <code>src/App.js</code> and save to reload added new data to check build add again.
         </p>
         <a
           className="App-link"
